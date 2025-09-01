21ed3e28a5d0cb925143b007ca1f95be17a13826
Mel Cadano <mel.cdn@outlook.ph>
2025-09-01 20:58:30 +0800

    [TF AWS] Use S3 and DynamoDB as TF state

--- infra/terraform/aws/backend.tf
+++ infra/terraform/aws/backend.tf
@@ -1,14 +1,14 @@
 terraform {
-  # backend "s3" {
-  #   bucket         = "mel-pg-terraform-state"
-  #   key            = "terraform.tfstate"
-  #   # dynamodb_table = "mel-pg-terraform-state-lock"
-  #   region         = "ap-southeast-1"
-  #   # assume_role = {
-  #   #   role_arn = "arn:aws:iam::x:role/xt/x-terraform-state-role"
-  #   # }
-  #   workspace_key_prefix =  "dm-inventory"
-  # }
+  backend "s3" {
+    region = "ap-southeast-1"
+    key    = "default.tfstate"
+
+    # Will be populated on deploy or add it manually here
+    bucket         = ""
+    dynamodb_table = ""
+
+    workspace_key_prefix = "dm-inventory"
+  }
 
   required_providers {
     aws = {
